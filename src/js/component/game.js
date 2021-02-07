360b436f89c79e51e56bac9ff581c3d5b0192c36
hexbreak <theownerproject@gmail.com>
2021-02-07 05:00:38 +0000

    added hooks on game component

--- src/js/component/game.js
+++ src/js/component/game.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-export const Game = () => (
+export const Game = props => (
+    const [ platform, setPlatform ] = useState();
 	<div className="container">
-		<div className="H1">H1 Game Header Test!</div>
+		<div className="h1 text-center">H1 Game Header Test!</div>
+		<div className="">123</div>
 	</div>
 );
+
+Game.propTypes = {
+
+}
