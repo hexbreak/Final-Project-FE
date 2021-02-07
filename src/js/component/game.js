46cb55069a600b2673a05caa4b2fa849827cc039
hexbreak <theownerproject@gmail.com>
2021-02-07 05:09:19 +0000

    create const and props

--- src/js/component/game.js
+++ src/js/component/game.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
-export const Game = props => (
-    const [ platform, setPlatform ] = useState();
-	<div className="container">
-		<div className="h1 text-center">H1 Game Header Test!</div>
-		<div className="">123</div>
-	</div>
-);
+export const Game = props => {
+	const [platform, setPlatform] = useState();
 
-Game.propTypes = {
-
-}
+	return (
+		<div className="container">
+			<div className="h1 text-center">H1 Game Header Test!</div>
+			<div className="">123</div>
+		</div>
+	);
+};
+Game.propTypes = {};
