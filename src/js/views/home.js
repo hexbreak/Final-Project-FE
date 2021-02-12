b24678e9045916f64e0c1139e3effe7cc2bf438b
99lalo <xx66laloxx@gmail.com>
2021-02-12 21:01:43 +0000

    Update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,23 +1,25 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
 import "../../styles/home.scss";
 import { Game } from "../component/game";
-import { Footer } from "../component/footer";
-
+import { GameCarrousel } from "../component/gameCarrousel";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 	actions.loadGameList(1);
+	if (store.gameList != []) {
+		return (
+			<Carousel>
+				{store.gameList.forEach((game, index) => {
+					console.log(game);
 
-	console.log(store.gameList);
-	return (
-		<div className="container-fluid">
-			<div className="jumbotron">
-				<img src={store.loadGameList[0].background_image} alt="game_image" />
-				<h1>{store.loadGameList[0].name}</h1>
-				<h6>{actions.loadGame(store.loadGameList[0].id).description}</h6>
-			</div>
-		</div>
-	);
+					return <GameCarrousel game={game} key={index} />;
+				})}
+			</Carousel>
+		);
+	} else {
+		return <h1>Loading...</h1>;
+	}
 };
