6670d2eb378fee51c2331d6a7c854fe756799253
hexbreak <theownerproject@gmail.com>
2021-02-15 20:12:17 +0000

    added more layer structure for profile for functions

--- src/js/views/userProfile.js
+++ src/js/views/userProfile.js
@@ -1,17 +1,29 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserFavoriteList } from "../component/userFavoriteList";
-import PropTypes from "prop-types";
 import { Container, Row, Col } from "react-bootstrap";
+import { Card, CardImg, CardFooter } from "react-bootstrap";
+import { ListGroup } from "react-bootstrap";
+import PropTypes from "prop-types";
 
 export const UserProfile = () => {
 	return (
 		<Container>
 			<Row>
-				<Col xs={12} md={8}>
-					<div className="h1 text-center">H1, Profile Page Header.</div>
+				<Col md={{ span: 3, offset: 0 }}>
+					<Card>
+						<Card.Img src="https://reactstrap.github.io/assets/318x180.svg" width="100%" />
+						<Card.Footer>User Name</Card.Footer>
+					</Card>
 				</Col>
-				<Col xs={6} md={4}>
+				<Col md={{ span: 8, offset: 1 }}>
+					<ListGroup>
+						<ListGroup.Item>Test!</ListGroup.Item>
+						<ListGroup.Item>Test!</ListGroup.Item>
+						<ListGroup.Item>Test!</ListGroup.Item>
+						<ListGroup.Item>Test!</ListGroup.Item>
+						<ListGroup.Item>Test!</ListGroup.Item>
+					</ListGroup>
 					<div className="text-center">test two here!</div>
 				</Col>
 			</Row>
