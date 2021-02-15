3dc6a541f7b93e436b40a9a7b08e36f14b73d7d4
hexbreak <theownerproject@gmail.com>
2021-02-15 18:56:35 +0000

    fixing react-bootstrap for profile

--- src/js/views/userProfile.js
+++ src/js/views/userProfile.js
@@ -2,16 +2,19 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserFavoriteList } from "../component/userFavoriteList";
 import PropTypes from "prop-types";
+import { Container, Row, Col } from "react-bootstrap";
 
 export const UserProfile = () => {
 	return (
-		<div className="container">
-			<div className="h1 text-center">H1, Profile Page Header.</div>
-			<div className="row">
-				<div className="col-" />
-				<UserFavoriteList />
-				<div className="text-center">test two here!</div>
-			</div>
-		</div>
+		<Container>
+			<Row>
+				<Col xs={12} md={8}>
+					<div className="h1 text-center">H1, Profile Page Header.</div>
+				</Col>
+				<Col xs={6} md={4}>
+					<div className="text-center">test two here!</div>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
