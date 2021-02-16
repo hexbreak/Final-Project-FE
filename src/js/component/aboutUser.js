b5dc802ae44c4c72dd27ff8610375dcec99a0477
hexbreak <theownerproject@gmail.com>
2021-02-16 00:47:39 +0000

    profile changes

---
+++ src/js/component/aboutUser.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const AboutUser = () => {
+	return (
+		<div className="container">
+			<div className="about-me">About me, etc</div>
+		</div>
+	);
+};
--- src/js/views/userProfile.js
+++ src/js/views/userProfile.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import { UserFavoriteList } from "../component/userFavoriteList";
 import { Container, Row, Col } from "react-bootstrap";
 import { Card, CardImg, CardFooter } from "react-bootstrap";
-import { ListGroup } from "react-bootstrap";
+import { AboutUser } from "../component/aboutUser.js";
 import PropTypes from "prop-types";
 
-export const UserProfile = () => {
+export const UserProfile = props => {
+	const { store, actions } = useContext(Context);
+	console.log(store);
+	actions.loadGameList(2); // page 2
 	return (
 		<Container>
 			<Row>
@@ -17,14 +21,19 @@ export const UserProfile = () => {
 					</Card>
 				</Col>
 				<Col md={{ span: 8, offset: 1 }}>
-					<ListGroup>
-						<ListGroup.Item>Test!</ListGroup.Item>
-						<ListGroup.Item>Test!</ListGroup.Item>
-						<ListGroup.Item>Test!</ListGroup.Item>
-						<ListGroup.Item>Test!</ListGroup.Item>
-						<ListGroup.Item>Test!</ListGroup.Item>
-					</ListGroup>
+					<div className="list-group text-primary">
+						<div className="list-group-item active">1. Currently Played Game ID goes here!</div>
+						<div className="list-group-item">{store.gameList[0].name}</div>
+						<div className="list-group-item">{store.gameList[3].name}</div>
+						<div className="list-group-item">{store.gameList[7].name}</div>
+						<div className="list-group-item">{store.gameList[10].name}</div>
+					</div>
 					<div className="text-center">test two here!</div>
+				</Col>
+			</Row>
+			<Row>
+				<Col>
+					<AboutUser />
 				</Col>
 			</Row>
 		</Container>
