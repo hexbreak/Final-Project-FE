973ce9bf33896f1596cf66c620571d968c12c8cb
hexbreak <theownerproject@gmail.com>
2021-02-22 03:31:53 +0000

    added context and props in nowplaying comp

--- src/js/component/userNowPlaying.js
+++ src/js/component/userNowPlaying.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import PropTypes from "prop-types";
 
 export const UserNowPlaying = () => {
+	const { store, actions } = useContext(Context);
 	return (
 		<div className="list-group text-primary">
 			<div className="list-group-item active">1. Currently Played Game ID goes here!</div>
