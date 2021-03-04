b9a71e5dfa05f9d1bc15ade8829ef8cae48bfa59
99lalo <xx66laloxx@gmail.com>
2021-03-04 17:41:03 +0000

    Update

--- src/js/views/userProfileUpdate.js
+++ src/js/views/userProfileUpdate.js
@@ -25,15 +25,11 @@ export const UserProfileUpdate = props => {
 	const [platform1, setPlatform1] = useState(store.user.platforms[0]);
 	const [platform2, setPlatform2] = useState(store.user.platforms[1]);
 	const [platform3, setPlatform3] = useState(store.user.platforms[2]);
-	const [isNowPlaying1, setIsNowPlaying1] = useState(store.user.playing[0]);
-	const [isNowPlaying2, setIsNowPlaying2] = useState(store.user.playing[1]);
-	const [isNowPlaying3, setIsNowPlaying3] = useState(store.user.playing[2]);
-	const [isNowLooking1, setIsNowLooking1] = useState("");
-	const [isNowLooking2, setIsNowLooking2] = useState("");
-	const [isNowLooking3, setIsNowLooking3] = useState("");
-	var gamesFound1 = null;
-	var gamesFound2 = null;
-	var gamesFound3 = null;
+	const [playing1, setPlaying1] = useState(store.user.playing[0]);
+	const [playing2, setPlaying2] = useState(store.user.playing[1]);
+	const [playing3, setPlaying3] = useState(store.user.playing[2]);
+	const [isShowing, setIsShowing] = useState("");
+	const [gamesFound, setGamesFound] = useState("");
 	useEffect(() => {
 		const loadSearch = () => {
 			actions.loadTags("40");
@@ -41,9 +37,23 @@ export const UserProfileUpdate = props => {
 		};
 		loadSearch();
 	}, []);
-	const handleLook1 = gameName => {
-		setIsNowLooking1(gameName);
-		return (gamesFound1 = actions.looking(gameName));
+	const handleLook = gameName => {
+		setIsLooking(gameName);
+		fetch(`https://api.rawg.io/api/games?search=${gameName}&page_size=6`)
+			.then(function(response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				// Read the response as json.
+				return response.json();
+			})
+			.then(function(responseAsJson) {
+				// Do stuff with the JSON
+				return setGamesFound(responseAsJson.results);
+			})
+			.catch(function(error) {
+				console.log("Looks like there was a problem: \n", error);
+			});
 	};
 	return (
 		<Container>
@@ -73,7 +83,11 @@ export const UserProfileUpdate = props => {
 							<Form.Row>
 								<Form.Group as={Col} controlId="formGridPassword">
 									<Form.Label>About</Form.Label>
-									<Form.Control type="text" placeholder="About user..." value={store.user.about} />
+									<Form.Control
+										type="text"
+										placeholder="About user..."
+										defaultValue={store.user.about}
+									/>
 								</Form.Group>
 							</Form.Row>
 						</Form>
@@ -89,17 +103,22 @@ export const UserProfileUpdate = props => {
 						<Form>
 							<Form.Row>
 								<Dropdown>
-									<Dropdown.Toggle variant="secondary" id="dropdown-basic">
-										{isNowPlaying1.name}
+									<Dropdown.Toggle
+										variant="secondary"
+										id="dropdown-basic"
+										onClick={e => setIsShowing("1")}>
+										{playing1.name}
 									</Dropdown.Toggle>
 
-									<Dropdown.Menu>
-										<Dropdown.Item></Dropdown.Item>
-										{gamesFound1 != null &&
-											gamesFound1.map((value, index) => {
-												return <Dropdown.Item key={index}>{Action}</Dropdown.Item>;
-											})}
-									</Dropdown.Menu>
+									{isShowing == "1" && (
+										<Dropdown.Menu>
+											<Form.Control type="text" placeholder="About user..." />
+											{/* {gamesFound != null &&
+												gamesFound.map((value, index) => {
+													return <Dropdown.Item key={index}>{value.name}</Dropdown.Item>;
+												})} */}
+										</Dropdown.Menu>
+									)}
 								</Dropdown>
 
 								<Form.Group as={Col} controlId="formGridPassword">
@@ -107,7 +126,22 @@ export const UserProfileUpdate = props => {
 									<Form.Control
 										type="text"
 										placeholder="Something about the game"
-										value={store.user.playing[0] != null ? store.user.playing[0].notes : null}
+										defaultValue={store.user.playing[0] != null ? store.user.playing[0].notes : ""}
+									/>
+								</Form.Group>
+							</Form.Row>
+							<Form.Row>
+								<Form.Group as={Col} controlId="formGridEmail">
+									<Form.Label>Game</Form.Label>
+									<Form.Control type="text" placeholder="Game name..." />
+								</Form.Group>
+
+								<Form.Group as={Col} controlId="formGridPassword">
+									<Form.Label>Notes</Form.Label>
+									<Form.Control
+										type="text"
+										placeholder="Something about the game"
+										defaultValue={!!store.user.playing[1] ? store.user.playing[1].notes : ""}
 									/>
 								</Form.Group>
 							</Form.Row>
@@ -117,7 +151,7 @@ export const UserProfileUpdate = props => {
 									<Form.Control
 										type="text"
 										placeholder="Game name..."
-										value={isNowPlaying2.name != undefined ? isNowPlaying2.name : null}
+										defaultValue={store.user.playing[2] != null ? store.user.playing[2].name : null}
 									/>
 								</Form.Group>
 
@@ -126,26 +160,9 @@ export const UserProfileUpdate = props => {
 									<Form.Control
 										type="text"
 										placeholder="Something about the game"
-										value={store.user.playing[1] != null ? store.user.playing[1].notes : null}
-									/>
-								</Form.Group>
-							</Form.Row>
-							<Form.Row>
-								<Form.Group as={Col} controlId="formGridEmail">
-									<Form.Label>Game</Form.Label>
-									<Form.Control
-										type="text"
-										placeholder="Game name..."
-										value={store.user.playing[2] != null ? store.user.playing[2].name : null}
-									/>
-								</Form.Group>
-
-								<Form.Group as={Col} controlId="formGridPassword">
-									<Form.Label>Notes</Form.Label>
-									<Form.Control
-										type="text"
-										placeholder="Something about the game"
-										value={store.user.playing[2] != null ? store.user.playing[2].notes : null}
+										defaultValue={
+											store.user.playing[2] != null ? store.user.playing[2].notes : null
+										}
 									/>
 								</Form.Group>
 							</Form.Row>
@@ -159,90 +176,88 @@ export const UserProfileUpdate = props => {
 						<h5>Platforms</h5>
 					</Card.Header>
 					<Card.Body>
-						<Card.Text>
-							<Form>
-								<Form.Row>
-									<Form.Group as={Col} controlId="formGridEmail">
-										<Form.Label>Plaftform 1</Form.Label>
-										<DropdownButton
-											id="dropdown-basic-button"
-											variant="secondary"
-											title={platform1.name}>
-											{store.platforms[0] != undefined &&
-												store.platforms.map((value, index) => {
-													if (
-														value.name != platform1.name &&
-														value.name != platform2.name &&
-														value.name != platform3.name
-													) {
-														return (
-															<Dropdown.Item
-																key={index}
-																onClick={e =>
-																	setPlatform1({ id: value.id, name: value.name })
-																}>
-																{value.name}
-															</Dropdown.Item>
-														);
-													}
-												})}
-										</DropdownButton>
-									</Form.Group>
-									<Form.Group as={Col} controlId="formGridPassword">
-										<Form.Label>Plaftform 2</Form.Label>
-										<DropdownButton
-											id="dropdown-basic-button"
-											variant="secondary"
-											title={platform2.name}>
-											{store.platforms[0] != undefined &&
-												store.platforms.map((value, index) => {
-													if (
-														value.name != platform1.name &&
-														value.name != platform2.name &&
-														value.name != platform3.name
-													) {
-														return (
-															<Dropdown.Item
-																key={index}
-																onClick={e =>
-																	setPlatform2({ id: value.id, name: value.name })
-																}>
-																{value.name}
-															</Dropdown.Item>
-														);
-													}
-												})}
-										</DropdownButton>
-									</Form.Group>
-									<Form.Group as={Col} controlId="formGridEmail">
-										<Form.Label>Plaftform 3</Form.Label>
-										<DropdownButton
-											id="dropdown-basic-button"
-											variant="secondary"
-											title={platform3.name}>
-											{store.platforms[0] != undefined &&
-												store.platforms.map((value, index) => {
-													if (
-														value.name != platform1.name &&
-														value.name != platform2.name &&
-														value.name != platform3.name
-													) {
-														return (
-															<Dropdown.Item
-																key={index}
-																onClick={e =>
-																	setPlatform3({ id: value.id, name: value.name })
-																}>
-																{value.name}
-															</Dropdown.Item>
-														);
-													}
-												})}
-										</DropdownButton>
-									</Form.Group>
-								</Form.Row>
-							</Form>
-						</Card.Text>
+						<Form>
+							<Form.Row>
+								<Form.Group as={Col} controlId="formGridEmail">
+									<Form.Label>Plaftform 1</Form.Label>
+									<DropdownButton
+										id="dropdown-basic-button"
+										variant="secondary"
+										title={platform1.name}>
+										{store.platforms[0] != undefined &&
+											store.platforms.map((value, index) => {
+												if (
+													value.name != platform1.name &&
+													value.name != platform2.name &&
+													value.name != platform3.name
+												) {
+													return (
+														<Dropdown.Item
+															key={index}
+															onClick={e =>
+																setPlatform1({ id: value.id, name: value.name })
+															}>
+															{value.name}
+														</Dropdown.Item>
+													);
+												}
+											})}
+									</DropdownButton>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridPassword">
+									<Form.Label>Plaftform 2</Form.Label>
+									<DropdownButton
+										id="dropdown-basic-button"
+										variant="secondary"
+										title={platform2.name}>
+										{store.platforms[0] != undefined &&
+											store.platforms.map((value, index) => {
+												if (
+													value.name != platform1.name &&
+													value.name != platform2.name &&
+													value.name != platform3.name
+												) {
+													return (
+														<Dropdown.Item
+															key={index}
+															onClick={e =>
+																setPlatform2({ id: value.id, name: value.name })
+															}>
+															{value.name}
+														</Dropdown.Item>
+													);
+												}
+											})}
+									</DropdownButton>
+								</Form.Group>
+								<Form.Group as={Col} controlId="formGridEmail">
+									<Form.Label>Plaftform 3</Form.Label>
+									<DropdownButton
+										id="dropdown-basic-button"
+										variant="secondary"
+										title={platform3.name}>
+										{store.platforms[0] != undefined &&
+											store.platforms.map((value, index) => {
+												if (
+													value.name != platform1.name &&
+													value.name != platform2.name &&
+													value.name != platform3.name
+												) {
+													return (
+														<Dropdown.Item
+															key={index}
+															onClick={e =>
+																setPlatform3({ id: value.id, name: value.name })
+															}>
+															{value.name}
+														</Dropdown.Item>
+													);
+												}
+											})}
+									</DropdownButton>
+								</Form.Group>
+							</Form.Row>
+						</Form>
 					</Card.Body>
 				</Card>
 			</Row>
@@ -252,58 +267,54 @@ export const UserProfileUpdate = props => {
 						<h5>Highlights</h5>
 					</Card.Header>
 					<Card.Body>
-						<Card.Text>
-							<Form>
-								{store.user.platforms != null &&
-									store.user.platforms.map((value, index) => {
-										return (
-											<div key={index}>
-												<h6>{value.name}</h6>
-												<Form.Row>
-													<Form.Group as={Col} controlId="formGridEmail">
-														<Form.Label>Started</Form.Label>
-														<Form.Control
-															type="text"
-															placeholder="Game name..."
-															value={
-																store.user.game_progression[value.id].started != null
-																	? store.user.game_progression[value.id].started.name
-																	: null
-															}
-														/>
-													</Form.Group>
-													<Form.Group as={Col} controlId="formGridPassword">
-														<Form.Label>Finished</Form.Label>
-														<Form.Control
-															type="text"
-															placeholder="Game name..."
-															value={
-																store.user.game_progression[value.id].finished != null
-																	? store.user.game_progression[value.id].finished
-																			.name
-																	: null
-															}
-														/>
-													</Form.Group>
-													<Form.Group as={Col} controlId="formGridPassword">
-														<Form.Label>Completed</Form.Label>
-														<Form.Control
-															type="text"
-															placeholder="Game name..."
-															value={
-																store.user.game_progression[value.id].completed != null
-																	? store.user.game_progression[value.id].completed
-																			.name
-																	: null
-															}
-														/>
-													</Form.Group>
-												</Form.Row>
-											</div>
-										);
-									})}
-							</Form>
-						</Card.Text>
+						<Form>
+							{store.user.platforms != null &&
+								store.user.platforms.map((value, index) => {
+									return (
+										<div key={index}>
+											<h6>{value.name}</h6>
+											<Form.Row>
+												<Form.Group as={Col} controlId="formGridEmail">
+													<Form.Label>Started</Form.Label>
+													<Form.Control
+														type="text"
+														placeholder="Game name..."
+														defaultValue={
+															store.user.game_progression[value.id].started != null
+																? store.user.game_progression[value.id].started.name
+																: null
+														}
+													/>
+												</Form.Group>
+												<Form.Group as={Col} controlId="formGridPassword">
+													<Form.Label>Finished</Form.Label>
+													<Form.Control
+														type="text"
+														placeholder="Game name..."
+														defaultValue={
+															store.user.game_progression[value.id].finished != null
+																? store.user.game_progression[value.id].finished.name
+																: null
+														}
+													/>
+												</Form.Group>
+												<Form.Group as={Col} controlId="formGridPassword">
+													<Form.Label>Completed</Form.Label>
+													<Form.Control
+														type="text"
+														placeholder="Game name..."
+														defaultValue={
+															store.user.game_progression[value.id].completed != null
+																? store.user.game_progression[value.id].completed.name
+																: null
+														}
+													/>
+												</Form.Group>
+											</Form.Row>
+										</div>
+									);
+								})}
+						</Form>
 					</Card.Body>
 				</Card>
 			</Row>
