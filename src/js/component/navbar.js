d80f30060ca058e6426d40b0e06e27a7f9a0b1ce
99lalo <xx66laloxx@gmail.com>
2021-04-21 22:32:54 +0000

    Responsive Navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -22,93 +22,124 @@ export const Navbar = () => {
 	};
 	return (
 		<nav className="navbar mb-1 mt-1 container-fluid">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1" id="hover-logo">
-					Game
-					<i className="fas fa-gamepad m-2" />
-					Finder
-				</span>
-			</Link>
-			<Row>
-				<div className="dropdown">
-					<input
-						type="text"
-						className="form-control"
-						onChange={event => setGameName(event.target.value)}
-						onKeyDown={e => handleKeyDown(e)}
-						placeholder="Search..."
-						value={gameName}
-						aria-haspopup="true"
-						aria-expanded="false"
-						style={{ width: "50rem" }}
-					/>
-					{gameName != "" && <i className="fas fa-times float-right" onClick={e => setGameName("")} />}
-					{store.searchBar != undefined && gameName != "" && (
-						<div
-							id="myDropdown"
-							aria-labelledby="dropdownMenuButton"
-							className="dropdown-content"
-							style={{ display: "show", position: "absolute", zIndex: "300" }}>
-							{store.searchBar.map((value, index) => {
-								return (
-									<GameCard
-										id={"navCard"}
-										className="card"
-										key={index}
-										game={value}
-										size={"smallCard"}
-										cleanSearch={e => setGameName("")}
-									/>
-								);
-							})}
-						</div>
-					)}
-				</div>
-				<Link to="/search">
-					<button className="btn btn-light ml-5 pl-4 pr-4 searchButton" id="hover">
-						<i className="fas fa-search" />
-					</button>
-				</Link>
-			</Row>
+			<span className="navbar-brand mb-0 h1" onClick={e => history.push("/home")} id="hover-logo">
+				Game
+				<i className="fas fa-gamepad m-2" />
+				Finder
+			</span>
+			<div className="dropdown searchbar">
+				<input
+					type="text"
+					className="form-control"
+					onChange={event => setGameName(event.target.value)}
+					onKeyDown={e => handleKeyDown(e)}
+					placeholder="Search..."
+					value={gameName}
+					aria-haspopup="true"
+					aria-expanded="false"
+				/>
+				{gameName != "" && <i className="fas fa-times float-right" onClick={e => setGameName("")} />}
+				{store.searchBar != undefined && gameName != "" && (
+					<div
+						id="myDropdown"
+						aria-labelledby="dropdownMenuButton"
+						className="dropdown-content"
+						style={{ display: "show", position: "absolute", zIndex: "300" }}>
+						{store.searchBar.map((value, index) => {
+							return (
+								<GameCard
+									id={"navCard"}
+									className="card"
+									key={index}
+									game={value}
+									size={"smallCard"}
+									cleanSearch={e => setGameName("")}
+								/>
+							);
+						})}
+					</div>
+				)}
+			</div>
+			<button className="btn btn-light searchButton" onClick={e => history.push("/search")} id="hover">
+				<i className="fas fa-search" />
+			</button>
 			{store.id > 0 ? (
-				<div className="ml">
-					<Link to="/usergames">
-						<button
-							style={{ marginRight: "1rem" }}
-							className="btn btn-light pl-4 pr-4 navButton"
-							id="hover">
-							<span>My Games</span>
-						</button>
-					</Link>
-					<Link to="/editpreference">
-						<button
-							style={{ marginRight: "1rem" }}
-							className="btn btn-light pl-4 pr-4 navButton"
-							id="hover">
-							<span>Preferences</span>
-						</button>
-					</Link>
+				<div className="ml bigScreen">
+					<button
+						className="btn btn-light navButton navRight"
+						onClick={e => history.push("/usergames")}
+						id="hover">
+						<span>My Games</span>
+					</button>
+					<button
+						className="btn btn-light navButton navRight"
+						onClick={e => history.push("/userpreference")}
+						id="hover">
+						<span>Preferences</span>
+					</button>
 					<button className="btn btn-light navButton" onClick={e => actions.logout(history)} id="hover">
 						<span>Logout</span>
 					</button>
 				</div>
 			) : (
-				<div className="ml">
-					<Link to="/login">
-						<button
-							style={{ marginRight: "1rem" }}
-							onClick=""
-							className="btn btn-light navButton"
-							id="hover">
-							<span>Login</span>
-						</button>
-					</Link>
-					<Link to="/registration">
-						<button className="btn btn-light navButton" id="hover">
-							<span>Sign Up</span>
-						</button>
-					</Link>
+				<div className="ml bigScreen">
+					<button
+						className="btn btn-light navButton navRight"
+						onClick={e => history.push("/login")}
+						id="hover">
+						<span>Login</span>
+					</button>
+					<button className="btn btn-light navButton" onClick={e => history.push("/registration")} id="hover">
+						<span>Sign Up</span>
+					</button>
 				</div>
+			)}
+			{store.id > 0 ? (
+				<Dropdown className="smallScreen">
+					<Dropdown.Toggle variant="light" id="dropdown-basic">
+						<i className="fas fa-user-tie" />
+					</Dropdown.Toggle>
+					<Dropdown.Menu>
+						<Dropdown.Item
+							className="btn btn-light navButton"
+							id="hover"
+							onClick={e => history.push("/usergames")}>
+							My Games
+						</Dropdown.Item>
+						<Dropdown.Item
+							className="btn btn-light navButton"
+							id="hover"
+							onClick={e => history.push("/userpreference")}>
+							Preferences
+						</Dropdown.Item>
+						<Dropdown.Item
+							className="btn btn-light navButton"
+							id="hover"
+							onClick={e => actions.logout(history)}>
+							Something else
+						</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
+			) : (
+				<Dropdown className="smallScreen">
+					<Dropdown.Toggle variant="light" id="dropdown-basic">
+						<i className="fas fa-user" />
+					</Dropdown.Toggle>
+					<Dropdown.Menu>
+						<Dropdown.Item
+							className="btn btn-light navButton"
+							id="hover"
+							onClick={e => history.push("/login")}>
+							Login
+						</Dropdown.Item>
+						<Dropdown.Item
+							className="btn btn-light navButton"
+							id="hover"
+							onClick={e => history.push("/registration")}>
+							Register
+						</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 			)}
 		</nav>
 	);
